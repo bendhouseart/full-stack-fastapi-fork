d8c8bc80226c23129b91e8b9585ccdf8f6c0f490
Anthony Galassi <2317139-anthonygalassi@users.noreply.gitlab.com>
2020-10-12 22:27:41 -0700

    smoothing out single instance

--- {{cookiecutter.project_slug}}/terraform_single_instance/securitygroup.tf
+++ {{cookiecutter.project_slug}}/terraform_single_instance/securitygroup.tf
@@ -1,21 +1,34 @@
-//resource "aws_security_group" "allow-ssh" {
-//  vpc_id      = var.vpc_id
-//  name        = "allow-ssh"
-//  description = "security group that allows ssh and all egress traffic"
-//  egress {
-//    from_port   = 0
-//    to_port     = 0
-//    protocol    = "-1"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//
-//  ingress {
-//    from_port   = 22
-//    to_port     = 22
-//    protocol    = "tcp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//  tags = {
-//    Name = "allow-ssh"
-//  }
-//}
+resource "aws_security_group" "this" {
+  //count = var.create_sg && var.create ? 1 : 0
+  vpc_id = var.vpc_id == "" ? null : var.vpc_id
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "public_ports" {
+  count = var.create_sg && var.create ? length(var.public_ports) : 0
+
+  type              = "ingress"
+  security_group_id = join("", aws_security_group.this.*.id)
+  protocol          = "tcp"
+  from_port         = var.public_ports[count.index]
+  to_port           = var.public_ports[count.index]
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "private_ports" {
+  count = var.create_sg && var.create ? length(var.private_ports) : 0
+
+  type              = "ingress"
+  security_group_id = join("", aws_security_group.this.*.id)
+  protocol          = "tcp"
+  from_port         = var.private_ports[count.index]
+  to_port           = var.private_ports[count.index]
+  cidr_blocks       = var.private_port_cidrs
+}
